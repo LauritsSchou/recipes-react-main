2f9bc6d92c0477f0b10f0f45031cd99ca06081f0
LauritsSchou <laurits.schou@gmail.com>
2024-03-01 16:15:32 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { Route, Routes } from "react-router-dom";
+import { Categories } from "./recipes/Categories";
+import Recipe from "./recipes/Recipe";
+import RecipeForm from "./recipes/RecipeForm";
+import RecipesLayout from "./recipes/RecipesLayout";
+import LogIn from "./security/Login";
+import LogOut from "./security/LogOut";
+import Layout from "./Layout";
+import Home from "./Home";
+import "./App.css";
+import RequireAuth from "./security/RequireAuth";
+// import { useAuth } from "./security/AuthProvider";
+
+export default function App() {
+  // const auth = useAuth();
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories/" element={<Categories />} />
+        <Route path="/recipes" element={<RecipesLayout />}>
+          <Route path=":id" element={<Recipe />} />
+        </Route>
+        <Route
+          path="/add"
+          element={
+            <RequireAuth roles={["ADMIN"]}>
+              <RecipeForm />
+            </RequireAuth>
+          }
+        />
+
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/logout" element={<LogOut />} />
+        <Route path="*" element={<h2>Not Found</h2>} />
+      </Routes>
+    </Layout>
+  );
+}
---
+++ src/security/Login.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { useAuth } from "./AuthProvider";
+import { User } from "../services/authFacade";
+import { useNavigate } from "react-router-dom";
+import "./login.css";
+
+const Login = () => {
+  const [user, setUser] = useState({ username: "", password: "" });
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const auth = useAuth();
+
+  const from = location.state?.from?.pathname || "/";
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    console.log("handleSubmit");
+
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const user = Object.fromEntries(formData) as unknown as User;
+    console.log(user);
+
+    auth.signIn(user).then(() => {
+      navigate(from, { replace: true });
+    });
+  }
+
+  return (
+    <div className="login-wrapper">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div className="login-form-group">
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" value={user.username} onChange={(e) => setUser((prev) => ({ ...prev, username: e.target.value }))} required />
+        </div>
+        <div className="login-form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" value={user.password} onChange={(e) => setUser((prev) => ({ ...prev, password: e.target.value }))} required />
+        </div>
+        <button type="submit" className="login-btn">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
